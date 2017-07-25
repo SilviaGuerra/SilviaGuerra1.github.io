1c6b64c35537b58d4f81dfae14e84b26d2cd5b71
SilviaGuerra <silvia.guerrahdz@gmail.com>
2017-07-25 14:44:55 -0500

    inicie un nuevo portafolio con materialize

---
+++ js/main.js
@@ -0,0 +1,15 @@
+  // // init controller
+  // var controller = new ScrollMagic.Controller({
+  //   globalSceneOptions: {triggerHook: "onCenter"}
+  // });
+  // // init scene
+  // var scene = new ScrollMagic.Scene({
+  //   duration: 300,
+  //   offset: 100
+  // })
+  // .addTo(controller)
+  // .addIndicators();
+
+$(document).ready(function(){
+  $(".button-collapse").sideNav();
+});
